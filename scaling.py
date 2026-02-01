90727072cc63d5eabf7bea9514bfd3551013cad0
anukawan04 <anukawan2024@gmail>
2026-02-01 08:17:02 +0545

    scalinb

---
+++ scaling.py
@@ -0,0 +1,15 @@
+# scaling: it ensures that all features contribute equally to the model
+# by bringing them to a common scale. improving model performance,
+#  for distance-based algorithms. z= (x- mean)/s.d
+
+
+
+
+
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+X = np.array([[1, 2], [3, 4], [5, 6]])
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+print(X_scaled)
